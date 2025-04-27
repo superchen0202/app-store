0bc9422ac92b3551b9d325660f411d6e0c1e9a96
superchen0202 <chensuper0202@gmail.com>
2025-04-28 01:00:30 +0800

    add redux-toolkit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,21 @@
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { decrement, increment } from '@/redux/counterSlice';
+
+export const Counter = () => {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <div>
+        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <span>{count}</span>
+        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style/style.css';
 import App from './App.tsx';
+import { store } from './redux/store.ts';
+import { Provider } from 'react-redux';
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root');
+
+if (!container)
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  );
+
+const root = createRoot(container);
+root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
